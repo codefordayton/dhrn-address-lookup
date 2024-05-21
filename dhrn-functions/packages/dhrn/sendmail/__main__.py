b2883ef9757ba5b442fd2e2c8df1de9ee19c3bea
Dave Best <dave.e.best@gmail.com>
2024-05-21 15:33:14 -0400

    making the addresses env variables.

--- dhrn-functions/packages/dhrn/sendmail/__main__.py
+++ dhrn-functions/packages/dhrn/sendmail/__main__.py
@@ -4,13 +4,15 @@ from jinja2 import Template
 
 def main(args):
     MAILTRAP_API_TOKEN = os.environ.get("MAILTRAP_API_TOKEN")
+    MAILTRAP_SENDER_ADDRESS = os.environ.get("MAILTRAP_SENDER_ADDRESS") 
+    MAILTRAP_TO_ADDRESS = os.environ.get("MAILTRAP_TO_ADDRESS")
 
     with open("template.html") as f:
         template = Template(f.read())
 
     mail = mt.Mail(
-        sender=mt.Address(email="mailtrap@demomailtrap.com", name="Mailtrap Test"),
-        to=[mt.Address(email="dave.e.best@gmail.com")],
+        sender=mt.Address(email=MAILTRAP_SENDER_ADDRESS, name="Code For Dayton"),
+        to=[mt.Address(email=MAILTRAP_TO_ADDRESS)],
         subject="New submission from the DHRN Screener",
         text="Please enable HTML to view this message",
         html=template.render(data=args),
