0da5ebfa0fa426c19c8a6ff9f1b5a6b47acf585c
Dave Best <dave.e.best@gmail.com>
2024-05-20 20:17:25 -0400

    WIP email sending

--- dhrn-functions/packages/dhrn/sendmail/__main__.py
+++ dhrn-functions/packages/dhrn/sendmail/__main__.py
@@ -1,12 +1,15 @@
+import os
 import mailtrap as mt
 
 def main(args):
+    MAILTRAP_API_TOKEN = os.environ.get("MAILTRAP_API_TOKEN")
+
     mail = mt.Mail(
         sender=mt.Address(email="mailtrap@demomailtrap.com", name="Mailtrap Test"),
         to=[mt.Address(email="dave.e.best@gmail.com")],
-        subject="You are awesome!",
+        subject="New submission from the DHRN Screener",
         text="Congrats for sending test email with Mailtrap!",
         category="Integration Test",
     )
-    client = mt.MailtrapClient(token="REPLACE_ME")
+    client = mt.MailtrapClient(token=MAILTRAP_API_TOKEN)
     client.send(mail)
